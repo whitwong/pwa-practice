af282aac0308bdeebd40f5e9fa776350e4e8474f
Whitney Wong <whitney.wong@utexas.edu>
2020-07-26 18:09:34 -0500

    Lesson 16 - Fetch Cached Assets. To the fetch event, added a custom event to return cached asset if fetch request matched the cached resource. Otherwise, resume initial fetch request to the server. Updated README with notes on getting cached assets via fetch event in service worker.

--- sw.js
+++ sw.js
@@ -9,6 +9,7 @@ const assets = [
   '/css/materialize.min.css',
   '/img/dish.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v53/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
 // install service worker
@@ -32,4 +33,12 @@ self.addEventListener('activate', evt => {
 // fetch event
 self.addEventListener('fetch', evt => {
   // console.log('fetch', evt);
+  // resondWith() method allows us to pause fetch event and respond with a custom event.
+  evt.respondWith(
+    caches.match(evt.request)  // Check to see if there's a resource in the app cache that matches event object (evt) request.
+      .then(cacheRes => {
+        // If there is a match, then asset is returned. If not a match, then an empty response is returned so fetch request from the server.
+        return cacheRes || fetch(evt.request);
+      })
+  )
 });
