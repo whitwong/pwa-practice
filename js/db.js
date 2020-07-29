993aa20e9d86ac36b4c8703577c78c7c27a81f06
Whitney Wong <whitney.wong@utexas.edu>
2020-07-28 22:05:41 -0500

    Lesson 25 - Updating UI. Added function to ui.js to add recipes data from Firebase db to the DOM. Used query selector on recipes class to specify where to add the html. Removed dummy recipe html from index.html. Called new function in db.js to pass in db data returned from db snapshot.

--- js/db.js
+++ js/db.js
@@ -7,6 +7,7 @@ db.collection('recipes').onSnapshot(snapshot => {
     // console.log(change, change.doc.data(), change.doc.id);
     if(change.type === "added") {
       // add the document data to the web page
+      renderRecipe(change.doc.data(), change.doc.id)
     }
     if(change.type === "removed") {
       // remove the document data from the web page
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,6 @@
+const recipes = document.querySelector('.recipes');
+
+// Event listeners to enable side menu and form to show when selected
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu. M --> Materialize reference
   const menus = document.querySelectorAll('.side-menu');
@@ -6,3 +9,22 @@ document.addEventListener('DOMContentLoaded', function() {
   const forms = document.querySelectorAll('.side-form');
   M.Sidenav.init(forms, {edge: 'left'});
 });
+
+// Render recipe data
+const renderRecipe = (data, id) => {
+  const html = `
+    <div class="card-panel recipe white row" data-id="${id}">
+      <img src="/img/dish.png" alt="recipe thumb">
+      <div class="recipe-details">
+        <div class="recipe-title">${data.title}</div>
+        <div class="recipe-ingredients">${data.ingredients}</div>
+      </div>
+      <div class="recipe-delete">
+        <i class="material-icons" data-id="${id}">delete_outline</i>
+      </div>
+    </div>
+  `;
+
+  // Add recipes-html to the inner html of the recipes query selector
+  recipes.innerHTML += html;
+};
